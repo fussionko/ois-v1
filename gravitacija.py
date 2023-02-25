3422d86a9fe61a357210ce428de1a71e011b9735
fussionko <sebi.vidergar@gmail.com>
2023-02-25 12:21:47 +0100

    Izpis Visine #3

--- gravitacija.py
+++ gravitacija.py
@@ -1,10 +1,8 @@
-print("OIS je zakon!")
-
 C = 6.674 * pow(10, -11)
 M = 5.972 * pow(10, 24)
 r = 6.371 * pow(10, 6)
 
 height = int(input())
 a = (C * M) / pow(r + height, 2)
-
+print(height)
 print(a)
